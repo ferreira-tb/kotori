7105da1af04c7dd41d372e31d9fdcd3cb043a1dd
Andrew Ferreira <andrew.shien2@gmail.com>
2023-10-24 18:25:03 -0300

    chore: test

--- renderer/database/index.ts
+++ renderer/database/index.ts
@@ -6,5 +6,5 @@ export const sequelize = new Sequelize({
 	dialect: 'sqlite',
 	storage: join(ipcRenderer.sendSync('app(sync):user-data'), 'kotori.db'),
 	logging: false,
-models: []
+	models: []
 });
