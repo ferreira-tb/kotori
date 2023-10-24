589f94d3066886446dd3f992ca399552ad32eb11
Andrew Ferreira <andrew.shien2@gmail.com>
2023-10-24 18:52:06 -0300

    chore: config prettier

--- renderer/components/TheMainMenu.vue
+++ renderer/components/TheMainMenu.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="flex items-center justify-center"></div>
+	<div class="flex select-none items-center justify-start gap-8 p-1">
+		<div>Library</div>
+	</div>
 </template>
